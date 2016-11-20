df039f2312af3fe0d6fad80d98e8dd18c0f92237
licylk <1036814127@qq.com>
2016-11-20 16:49:18 +0800

    简单的博客实现

---
+++ microblog/routes/index.js
@@ -0,0 +1,134 @@
+var crypto = require('crypto'),
+    User = require('../models/user.js'),
+	Post = require("../models/post.js");
+
+module.exports = function(app){
+	
+  app.get("/",function(req,res){
+	  Post.get(null,function(err,posts){
+		if(err){
+		  posts=[];	
+		}  
+		res.render("index",{title:"首页",posts:posts}); 
+	  }) 
+  })
+  app.get("/reg",checkNotLogin);
+  app.get('/reg',function(req,res){
+	 res.render('reg',{title:"用户注册"});  
+  });
+  app.post("/reg",checkNotLogin);
+  app.post("/reg",function(req,res){
+	  if(req.body['password'].length<6||req.body['password-repeat'].length<6){
+		  req.flash("error","设置的密码长度小于6位，请重新设置");
+		  return res.redirect('/reg');  
+	  }
+	  if(req.body['password-repeat']!=req.body['password']) {
+		req.flash('error','两次输入的口令不一致');
+		return res.redirect('/reg');  
+	  }
+	  
+	  var md5 = crypto.createHash('md5');
+	  var password=md5.update(req.body.password).digest('base64');
+	  
+	  var newUser = new User({
+		 name:req.body.username,
+		 password:password,  
+	  });
+	  
+	  User.get(newUser.name,function(err,user){
+		if(user){
+		   err="user has already exists.";
+		}
+		if(err){
+		  req.flash("error",err);
+		  return res.redirect("/reg");	
+		}  
+		newUser.save(function(err){
+	     if(err){
+		   req.flash('error',err);
+		   return res.redirect('/reg');	 
+		 }
+		 req.session.user=newUser;
+		 req.flash('success','注册成功');
+		 res.redirect("/");		
+		})
+	  })
+  });
+  app.get("/login",checkNotLogin);
+  app.get("/login",function(req,res){
+	 res.render("login",{title:"用户登入"});  
+  });
+  app.post("/login",checkNotLogin);
+  app.post("/login",function(req,res){
+	 var md5 = crypto.createHash("md5");
+	 var password = md5.update(req.body.password).digest("base64");
+	 
+	 User.get(req.body.username,function(err,user){
+	 if(!user){
+	   req.flash("error","用户不存在");
+	   return res.redirect("/login");	 
+	 }	 
+	 if(user.password!=password){
+	   req.flash("error","用户密码错误");
+	   return res.redirect("/login");	 
+	 }
+	 req.session.user=user;
+	 req.flash("success","登入成功");
+	 res.redirect("/")
+	 }) 
+  });
+  app.get("/logout",checkLogin);
+  app.get("/logout",function(req,res){
+	 req.session.user=null;
+	 req.flash("success","登出成功");
+	 res.redirect("/"); 
+  })
+  
+  app.get("/u/:user",function(req,res){
+	User.get(req.params.user,function(err,user){
+	  if(!user){
+		req.flash("error","用户不存在");
+		return res.redirect("/");  
+	  }	
+	  Post.get(user.name,function(err,posts){
+		if(err){
+		  req.flash("error".err);
+		  return res.redirect("/");	
+		}  
+		res.render("user",{
+		  title:user.name,
+		  posts:posts	
+		})
+	  })
+	})  
+  })
+  app.post('/post', checkLogin);
+  app.post('/post', function(req, res) {
+    var currentUser = req.session.user;
+    var post = new Post(currentUser.name, req.body.post);
+    post.save(function(err) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      req.flash('success', '发表成功');
+      res.redirect('/u/' + currentUser.name);
+    });
+  });
+};
+
+function checkLogin(req,res,next){
+  if(!req.session.user){
+	req.flash("error","未登入");
+	return res.redirect("/login");  
+  }
+  next();
+}
+
+function checkNotLogin(req,res,next){
+  if(req.session.user){
+	req.flash("error","已登入");
+	return res.redirect("/");  
+  }	
+  next();
+}
---
+++ microblog/settings.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+//保存数据库的链接信息
+module.exports = {
+	cookieSecret:"microblog",
+	db:"microblog",
+	host:"localhost",
+	port:27017
+	};
+	
+	
